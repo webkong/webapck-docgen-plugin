70aeaef98f1df854cd2df7dce332461960275f79
wangsw <wangsw@ushareit.com>
2019-02-14 16:11:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,99 @@
+# webpack docgen plugin
+
+Automatically generate documentation for js function.
+
+## Install
+
+```
+npm i -D webapck-docgen-pulgin
+```
+
+## Useage
+
+```
+
+  plugins: [
+    new DocGenPlugin({
+      docFile: 'list.md', // document file name
+      title: 'Utils list', // document title
+      description: 'Function & Modules list.', // document description
+      entry: './src/index.js' // string | [string] | object { <key>: string | string }  <files path>
+    })
+  ]
+
+```
+
+```
+
+  plugins: [
+    new DocGenPlugin({
+      dir: './src' // string < directory path >
+    })
+  ]
+
+```
+
+## Example
+
+```
+src
+├── date
+│   └── index.js
+├── index.js
+└── test
+    └── index.js
+```
+
+```
+//index.js
+
+/**
+ * @name format
+ * @description Converts time into the specified format
+ * @param {String|Number} time
+ * @param {String=} fmt
+ * @return {String}
+ */
+function format(time, fmt) {
+  fmt = fmt || 'yyyy-MM-dd';
+  time = timestamp(time);
+  let date = new Date(time);
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    let str = o[k] + '';
+    if (new RegExp(`(${k})`).test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+    }
+  }
+  return fmt;
+}
+```
+```
+list.md
+
+# Utils list
+
+ Function & Modules list.
+
+## home/index
+
+### format
+
+>Converts time into the specified format
+
+|params| type | required | description | 
+| ---- | ---- | ---- | ---- | 
+|time|String/Number|Yes| | 
+
+>return {String}
+
+```
---
+++ index.js
@@ -0,0 +1,148 @@
+const path = require('path');
+const fs = require('fs');
+const pluginName = 'WebpackDocGenPlugin';
+/**
+ *@description plugin for webpack to make javascript file docgen.
+ *@param options<Object> context:String, entry:Array, keys:Array
+ *@class selfWebpackPlugin
+ */
+class WebpackDocGenPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+
+  apply(compiler) {
+    compiler.hooks.emit.tap(pluginName, compilation => {
+      let list = '# ' + (this.options.title || 'Utils list') + '\n\n';
+      list += this.options.description || 'Function & Modules list.' + '\n\n';
+
+      let context, entry;
+
+      if (!this.options.context) {
+        context = compiler.options.context;
+      } else if (!path.isAbsolute(this.options.context)) {
+        context = path.join(compiler.options.context, this.options.context);
+      } else {
+        context = this.options.context;
+      }
+      if (!this.options.entry) {
+        entry = compiler.options.entry;
+      } else {
+        entry = this.options.entry;
+      }
+      if (!this.options.dir) {
+
+      } else {
+        const reg = /\.\S+$/;
+        let pagesPath = {};
+        let singleFiles = {};
+
+        const dir = this.options.dir;
+        fs.readdirSync(dir)
+          .map(filename => {
+            if (reg.test(filename)) {
+              singleFiles['home/' + filename.replace(/\.\S+/, '')] = dir + '/' + filename;
+            } else {
+              const jsReg = /\.js$/;
+              const pagePath = fs
+                .readdirSync(path.join(context, dir, './' + filename))
+                .filter(filename => jsReg.test(filename));
+              pagePath.map(name => {
+                pagesPath[filename] =
+                  dir + "/" + filename + "/" + name;
+              });
+            }
+
+          });
+
+        entry = {
+          ...singleFiles,
+          ...pagesPath
+        }
+      }
+
+      if (Object.prototype.toString.call(entry) === '[object Function]') {
+        throw new Error('not support dynamic entry')
+      }
+      const typeEntry = Object.prototype.toString.call(entry);
+      if (typeEntry === '[object String]') {
+        entry = [entry]
+      } else if (typeEntry === '[object Array]') {
+        entry = entry
+      } else if (typeEntry === '[object Object]') {
+        let temp = [];
+        Object.keys(entry).forEach((key) => {
+          temp.push({
+            name: key,
+            path: entry[key]
+          })
+        })
+        entry = temp;
+      }
+      if (entry) {
+        entry.forEach((elem) => {
+          let filePath = '';
+          let moduleName = '';
+          if (typeof (elem) === 'string') {
+            filePath = path.resolve(context, elem);
+            const lastName = filePath.match(/\/([a-z0-9A-z]*)\./)[1];
+            if (lastName === 'index') {
+              moduleName = filePath.match(/([a-z0-9A-z]+)\/[a-z0-9A-z]+\./)[1];
+            } else {
+              moduleName = lastName;
+            }
+          } else {
+            filePath = path.resolve(context, elem.path);
+            moduleName = elem.name
+          }
+          list += '## ' + moduleName + '\n\n';
+          const file = fs.readFileSync(filePath, {
+            encoding: 'UTF-8'
+          });
+
+          // const infoArr = file.match(/@name ([\w-]+)\n \* @description ([^\f\n\r\t\v]+)/g)
+          const infoArr = file.match(/\/\*(\s|.)*?\*\//g)
+
+          infoArr.forEach((block) => {
+            const arr = block.split('\n * ');
+            const name = arr[1].split(' ')[1];
+            const paramReg = /@param \{([\w|]+)\} (\w+)( - )?([\S ]+)?/;
+            if (arr[1].split(' ')[0] === '@name') {
+              list += '### ' + name + '\n\n';
+              list += arr[2].replace('@description ', '>') + '\n\n';
+              const params = arr.slice(3, -1);
+              if (params.length > 0) {
+                list += '|params| type | required | description | \n';
+                list += '| ---- | ---- | ---- | ---- | \n';
+                list += params.map((param) => {
+                  const paramArr = paramReg.exec(param);
+                  if (paramArr) {
+                    return ('|' + paramArr[2] + '|' + paramArr[1].replace('|', '/') + '|' + (paramArr[1].indexOf('=') > -1 ? 'No' : 'Yes') + '|' + (paramArr[4] || ' ') + '| \n\n');
+                  }
+                }).join('');
+              }
+              const returnStr = arr[arr.length - 1];
+              list += '>' + returnStr.replace('@', '').replace(/\n[ ]+\*\//, '')
+              list += '\n\n';
+            }
+          })
+          list += '\n\n';
+
+        })
+      } else {
+        throw new Error('not find entry')
+      }
+      compilation.assets[this.options.docFile || 'list.md'] = {
+        source() {
+          return list;
+        },
+        size() {
+          return list.length;
+        }
+      };
+    })
+  }
+}
+
+module.exports = WebpackDocGenPlugin;
